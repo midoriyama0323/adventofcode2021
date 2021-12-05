53a1c3c084865352e1e527c6c0526152dec7e384
Richard Illyés <midoriyama0323@gmail.com>
2021-12-05 13:35:34 +0100

    WinCheck done, game ongoing

--- Day 4/solution.R
+++ Day 4/solution.R
@@ -1,4 +1,26 @@
-draw <- readLines("Day 4/input.txt", n = 1)
+wincheck <- function(x) {
+  for (a in 1:length(x)) {
+    if (F %in% is.nan(colSums(x[[a]])) || F %in% is.nan(rowSums(x[[a]]))) {    
+      return(a)
+    }
+  }
+  return(FALSE)
+}
+
+game <- function(y) {
+  while (wincheck(gamestate) == F){
+    
+    
+    
+  }
+  
+  print(c(a, draw_pos))
+  
+}
+
+draw <- strsplit(readLines("Day 4/input.txt", n = 1), ",", T)
+
+draw <- as.numeric(draw[[1]])
 
 input <- read.table("Day 4/input.txt", skip = 1)
 
@@ -8,7 +30,11 @@ k <- 1
 
 for (i in 1:(length(input$V1)/5)){
 
-  cards[[i]] <- matrix(input[k:(k+4),], nrow = 1, ncol = 5)
+  cards[[i]] <- as.matrix(input[k:(k+4),])
 
   k <- k+5
 }
+
+gamestate <- lapply(1:100, matrix, data=NaN, nrow = 5, ncol = 5)
+
+
