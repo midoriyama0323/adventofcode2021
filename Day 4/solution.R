1889a63ddc4a8e45bf78bc9635c9525991137300
Richard Illyés <midoriyama0323@gmail.com>
2021-12-05 14:35:38 +0100

    solved part1

--- Day 4/solution.R
+++ Day 4/solution.R
@@ -1,21 +1,11 @@
 wincheck <- function(x) {
   for (a in 1:length(x)) {
     if (F %in% is.nan(colSums(x[[a]])) || F %in% is.nan(rowSums(x[[a]]))) {    
-      return(a)
+      a <<- a
+      return(T)
     }
   }
   return(FALSE)
-}
-
-game <- function(y) {
-  while (wincheck(gamestate) == F){
-    
-    
-    
-  }
-  
-  print(c(a, draw_pos))
-  
 }
 
 draw <- strsplit(readLines("Day 4/input.txt", n = 1), ",", T)
@@ -37,4 +27,16 @@ for (i in 1:(length(input$V1)/5)){
 
 gamestate <- lapply(1:100, matrix, data=NaN, nrow = 5, ncol = 5)
 
+draw_pos <- 1
 
+while (wincheck(gamestate) == F){
+  
+  for (i in 1:length(cards)){
+  gamestate[[i]][which(cards[[i]] == draw[draw_pos],arr.ind = T)] <- 
+    cards[[i]][which(cards[[i]] == draw[draw_pos],arr.ind = T)]
+  }
+  
+  draw_pos <- draw_pos + 1
+}
+
+result1 <- sum(cards[[a]][which(is.nan(gamestate[[a]]), arr.ind = T)], na.rm = T) * draw[draw_pos - 1]
