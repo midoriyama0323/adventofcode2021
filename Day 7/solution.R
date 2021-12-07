ba43126aed0483e7dff8066ae7269a6e73fd7e22
Richard Illyés <60649877+midoriyama0323@users.noreply.github.com>
2021-12-07 19:18:13 +0100

    formatting changes

--- Day 7/solution.R
+++ Day 7/solution.R
@@ -3,15 +3,14 @@ input <- scan("Day 7/input.txt", what = double(), sep = ",")
 result1 <- sum(abs(input - median(input)))
 
 result2 <-
-  min(c(sum((
-    abs(input - floor(mean(input))) * (abs(input - floor(mean(
-      input
-    ))) + 1)
-  ) / 2), sum((
-    abs(input - ceiling(mean(input))) * (abs(input - ceiling(mean(
-      input
-    ))) + 1)
-  ) / 2)))
+  min(
+    c(
+    sum((
+    abs(input - floor(mean(input))) * 
+    (abs(input - floor(mean(input))) + 1)) / 2),
+    sum((
+      abs(input - ceiling(mean(input))) * 
+         (abs(input - ceiling(mean(input))) + 1)) / 2)))
 
 print(paste("Result1:", result1), sep = "")
 print(paste("Result2:", result2), sep = "")
