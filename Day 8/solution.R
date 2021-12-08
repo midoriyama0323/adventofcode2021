06ef769cfca07a6de233d3d9847a58c8b7ea2429
Richard Illyés <60649877+midoriyama0323@users.noreply.github.com>
2021-12-08 22:44:53 +0100

    Day 8 in progress

--- Day 8/solution.R
+++ Day 8/solution.R
@@ -15,9 +15,33 @@ strsplit(
     scan("Day 8/input.txt", what = character(), sep = "\n", strip.white = T)
     , "\\W")
 
-part2 <- lapply(part2, function(x) {x[x != ""
-                                      ]})
+part2 <- as.data.frame(lapply(part2, function(x) {x[x != ""
+                                      ]}), col.names = 1:length(part2))
 
-test <- part2[which(nchar(part2) == 2, arr.ind = T)]
-
-lapply(part2, function(x) {x[which(nchar(x) == 2)]})
+nums <- data.frame(0:9)
+for (i in 1:length(part2)) {
+  part2[[i]] <- vapply(strsplit(part2[[i]], NULL), function(x) paste(sort(x), collapse = ''), '')
+  
+  nums[2,2] <- unique(part2[[i]][nchar(part2[[i]]) == 2])
+  nums[5,2] <- unique(part2[[i]][nchar(part2[[i]]) == 4])
+  nums[8,2] <- unique(part2[[i]][nchar(part2[[i]]) == 3])
+  nums[9,2] <- unique(part2[[i]][nchar(part2[[i]]) == 7])
+  seg6 <- unique(part2[[i]][nchar(part2[[i]]) == 6])
+  
+  for (f in 1:length(seg6)){
+    #ha az 1 es vonalai nincsenek a seg6ban, akkor a seg6 f-edik eleme a "6"
+    if (F %in% (unlist(strsplit(nums[2,2], NULL)) %in% unlist(strsplit(seg6[f], NULL)))){
+      nums[7,2] <- seg6[f]
+      seg6 <- seg6[seg6 != seg6[f]]
+    }
+  }
+  for (f in 1:length(seg6)){
+  if(!(F %in% (unlist(strsplit(nums[5,2], NULL)) %in% unlist(strsplit(seg6[f], NULL))))){
+    
+    nums[10,2] <- seg6[f]
+    seg6 <- seg6[seg6 != seg6[f]]
+  }
+  }
+  
+                                 
+}
