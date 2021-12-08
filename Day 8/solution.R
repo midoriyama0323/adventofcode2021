5d5f1c9ebe5698d41a5c53dc0fcecfd44f5e0f90
Richard Illyés <midoriyama0323@gmail.com>
2021-12-08 17:25:47 +0100

    part2 in progress

--- Day 8/solution.R
+++ Day 8/solution.R
@@ -1,3 +1,4 @@
+
 input <- trimws(scan("Day 8/input.txt", what = character(), sep = "|"), which = "both")
 
 part1 <- simplify2array(
@@ -9,11 +10,14 @@ part1 <- simplify2array(
 result1 <- sum(nchar(part1) %in% c(2,3,4,7))
 result1
 
-part2 <- as.data.frame(simplify2array(
-  strsplit(
+part2 <- 
+strsplit(
     scan("Day 8/input.txt", what = character(), sep = "\n", strip.white = T)
     , "\\W")
-))
-part2 <- part2[c(1:10,13:16),]
 
+part2 <- lapply(part2, function(x) {x[x != ""
+                                      ]})
 
+test <- part2[which(nchar(part2) == 2, arr.ind = T)]
+
+lapply(part2, function(x) {x[which(nchar(x) == 2)]})
