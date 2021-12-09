b132da2f7656b122a4f989ce69f584b5b951a90b
Richard Illyés <midoriyama0323@gmail.com>
2021-12-09 10:50:12 +0100

    part 2 solved

--- Day 8/solution.R
+++ Day 8/solution.R
@@ -17,7 +17,6 @@ strsplit(
 
 part2 <- as.data.frame(lapply(part2, function(x) {x[x != ""
                                       ]}), col.names = 1:length(part2))
-
 nums <- data.frame(0:9)
 for (i in 1:length(part2)) {
   part2[[i]] <- vapply(strsplit(part2[[i]], NULL), function(x) paste(sort(x), collapse = ''), '')
@@ -31,18 +30,55 @@ for (i in 1:length(part2)) {
   for (f in 1:length(seg6)){
     #ha az 1 es vonalai nincsenek a seg6ban, akkor a seg6 f-edik eleme a "6"
     #for some reason, this works for i = 2 and i = 5 but not for i = 1,3,4
-    if (F %in% (unlist(strsplit(nums[2,2], NULL)) %in% unlist(strsplit(seg6[f], NULL)))){
+    pos1 <- unlist(strsplit(nums[2,2], NULL))
+    if (length(intersect(pos1, unlist(strsplit(seg6[f], NULL)))) == 1){
+      topright <- setdiff(pos1, intersect(pos1, unlist(strsplit(seg6[f], NULL))))
       nums[7,2] <- seg6[f]
       seg6 <- seg6[seg6 != seg6[f]]
     }
   }
   for (f in 1:length(seg6)){
-  if(!(F %in% (unlist(strsplit(nums[5,2], NULL)) %in% unlist(strsplit(seg6[f], NULL))))){
+  if(length(intersect(unlist(strsplit(nums[5,2], NULL)), unlist(strsplit(seg6[f], NULL)))) == 4){
     
     nums[10,2] <- seg6[f]
     seg6 <- seg6[seg6 != seg6[f]]
+    nums[1,2] <- seg6[1]
+    
   }
+  }
+  seg5 <- unique(part2[[i]][nchar(part2[[i]]) == 5])
+  
+  for (f in 1:length(seg5)){
+    if ((length(intersect(topright, unlist(strsplit(seg5[f], NULL)))) == 0) && f <= length(seg5)){
+      nums[6,2] <- seg5[f]
+      seg5 <- seg5[seg5 != seg5[f]]
+    }
+    
+    
+  }
+  for (f in 1:length(seg5)){
+    pos1 <- unlist(strsplit(nums[2,2], NULL))
+    if (length(intersect(pos1, unlist(strsplit(seg5[f], NULL)))) == 1){
+      topright <- setdiff(pos1, intersect(pos1, unlist(strsplit(seg5[f], NULL))))
+      nums[3,2] <- seg5[f]
+      seg5 <- seg5[seg5 != seg5[f]]
+      nums[4,2] <- seg5[1]
+    }
   }
   
-                                 
+  
+  for (k in 1:length(part2[[i]])){
+    
+    if (!(is.numeric(part2[[i]][k]))) {
+      part2[[i]][k] <- nums[which(part2[[i]][k] == nums[,2]),1]}
+    
+    
+  }
+  res2[i] <- paste(part2[11:14,i], collapse = '')                               
 }
+
+result2 <- sum(as.numeric(res2))
+result2
+
+
+
